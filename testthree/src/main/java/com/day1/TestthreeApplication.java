d154564deb71311f8e27e59aed13b7b8fae52513
pavtestdummy <pavtestdummy@gmail.com>
2025-04-22 10:43:27 +0530

    first commit

---
+++ testthree/src/main/java/com/day1/TestthreeApplication.java
@@ -0,0 +1,14 @@
+package com.day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestthreeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("third one");
+		SpringApplication.run(TestthreeApplication.class, args);
+	}
+
+}
