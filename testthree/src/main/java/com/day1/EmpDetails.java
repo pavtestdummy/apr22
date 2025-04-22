cc23ab14750cb5f863d842a587e3a3af1a7566bd
pavtestdummy <pavtestdummy@gmail.com>
2025-04-22 11:50:04 +0530

    changes in pom

--- testthree/src/main/java/com/day1/EmpDetails.java
+++ testthree/src/main/java/com/day1/EmpDetails.java
@@ -2,7 +2,9 @@ package com.day1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class EmpDetails {
 
 	@GetMapping("/myapp/{empno}")
